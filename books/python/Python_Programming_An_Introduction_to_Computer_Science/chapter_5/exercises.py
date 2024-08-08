f78d38fbc117f077733f89947e8e643a394cdec9
DeadManLogan <grigoroudisdimosthenis@gmail.com>
2024-08-08 19:36:11 +0200

    Exercises 5.9 - 5.14

--- books/python/Python_Programming_An_Introduction_to_Computer_Science/chapter_5/exercises.py
+++ books/python/Python_Programming_An_Introduction_to_Computer_Science/chapter_5/exercises.py
@@ -64,4 +64,70 @@ def exercise_8():
             msg = msg + ch
     print(msg)
 
-exercise_8()
+def exercise_9():
+    sen = input("Enter words: ").split()
+    print(f"There are {len(sen)} words in your sentence.")
+
+def exercise_10():
+    sen = input("Enter words: ").split()
+
+    sum = 0
+    for w in sen:
+        sum += len(w) 
+    print(f"Average word lenght: {sum/len(sen)}")
+
+def exercise_11():
+    num1, num2 = input("Enter 2 numbers: ").split()
+    num1 = float(num1)
+    num2 = float(num2)
+    iterations = int(input("Enter the number of iterations: "))
+
+    print(f"index    {num1}      {num2}\n----------------------------------")
+    for i in range(iterations):
+        num1 = 3.9 * num1 * (1 - num1)
+        num2 = 3.9 * num2 * (1 - num2)
+        print(f"{iterations}    {num1}      {num2}")
+
+def exercise_12():
+    principal = float(input("Enter the starting investment: "))
+    apr = float(input("Enter the interst rate: "))
+    years = int(input("Enter the years to invest: "))
+
+    print("Year     Value\n--------------------")
+    for i in range(years):
+        principal += apr*principal
+        print(f"{i}     ${principal}")
+
+def exercise_13_12():
+    reader = open('chapter_5/exercise_material/input.txt', 'r')
+    writer = open('chapter_5/exercise_material/output.txt', 'w')
+    inputs = []
+
+    for line in reader:
+        inputs.append(float(line))
+
+    print("Year     Value\n--------------------", file=writer)
+    for i in range(int(inputs[2])):
+        inputs[0] += inputs[1]*inputs[0]
+        print(f"{i+1}     ${inputs[0]}", file=writer)
+    reader.close(), writer.close()
+
+def exercise_14():
+    file = input("Enter the path of the input file: ")
+    reader = open(file, 'r')
+
+    lines = 0
+    words = 0
+    characters = 0
+    for line in reader:
+        lines += 1
+        words_per_line = line.split()
+        words += len(words_per_line)
+        for ch in words_per_line:
+            characters += len(ch)
+
+    print(f"Lines: {lines}  Words: {words}  Characters: {characters}")
+
+
+
+exercise_14()
