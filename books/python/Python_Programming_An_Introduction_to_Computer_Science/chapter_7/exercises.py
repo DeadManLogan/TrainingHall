925996c7283ca63c779a43ae93f818cff8296766
DeadManLogan <grigoroudisdimosthenis@gmail.com>
2024-08-16 19:07:12 +0200

    If-else

--- books/python/Python_Programming_An_Introduction_to_Computer_Science/chapter_7/exercises.py
+++ books/python/Python_Programming_An_Introduction_to_Computer_Science/chapter_7/exercises.py
@@ -1,3 +1,5 @@
+import math
+
 def temperature():
     celsius = float(input("Enter temperature in Celsius: "))
     fahrenheit = 9/5 * celsius + 32
@@ -7,3 +9,21 @@ def temperature():
         print("HEAT")
     if fahrenheit < 30:
         print("COLD")
+
+def main():
+    print("This program finds the real solutions to a quadrantic\n")
+    a = float(input("Enter coefficient a: "))
+    b = float(input("Enter coefficient b: "))
+    c = float(input("Enter coefficient c: "))
+
+    discrim = b*b - 4*a*c
+    if discrim >= 0:
+        disc_root = math.sqrt(discrim)
+        root_1 = (-b + disc_root)/(2*a)
+        root_2 = (-b - disc_root)/(2*a)
+        print(f"Solutions: {root_1}, {root_2}")
+    else:
+        print("No real roots.")
+
+if __name__ == "__main__":
+    main()
