cd1ac890d8b7bb9a68f5284402867d9ef9727f4d
DeadManLogan <grigoroudisdimosthenis@gmail.com>
2024-08-18 22:57:09 +0200

    Chapter 7, first exercise

--- books/python/Python_Programming_An_Introduction_to_Computer_Science/chapter_7/exercises.py
+++ books/python/Python_Programming_An_Introduction_to_Computer_Science/chapter_7/exercises.py
@@ -1,32 +1,15 @@
-import math
+def exercise_1():
+    hours = float(input("Enter the hours you worked: "))
+    rate = float(input("Enter the hourly rate: "))
 
-def temperature():
-    celsius = float(input("Enter temperature in Celsius: "))
-    fahrenheit = 9/5 * celsius + 32
-    print(f"Fahrenheit: {fahrenheit}")
+    try:
+        if hours <= 40:
+            week_wage = hours * rate
+            print(f"Your weekly wage is: {week_wage}")
+        else:
+            week_wage = 40 * rate + (hours - 40) * rate * 1.5
+            print(f"Your weekly wage is: {week_wage}")
+    except ValueError as error:
+        print(error)
 
-    if fahrenheit > 90:
-        print("HEAT")
-    if fahrenheit < 30:
-        print("COLD")
-
-def main():
-    print("This program finds the real solutions to a quadrantic\n")
-    a = float(input("Enter coefficient a: "))
-    b = float(input("Enter coefficient b: "))
-    c = float(input("Enter coefficient c: "))
-
-    discrim = b*b - 4*a*c
-    if discrim > 0:
-        disc_root = math.sqrt(discrim)
-        root_1 = (-b + disc_root)/(2*a)
-        root_2 = (-b - disc_root)/(2*a)
-        print(f"Solutions: {root_1}, {root_2}")
-    elif discrim == 0:
-        root = -b/(2*a)
-        print(f"Double root: {root}")
-    else:
-        print("No real roots.")
-
-if __name__ == "__main__":
-    main()
+exercise_1()
