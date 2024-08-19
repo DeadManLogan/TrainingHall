c03b5ec9defc019bb0e1599da8287a89d89cf8f5
DeadManLogan <grigoroudisdimosthenis@gmail.com>
2024-08-19 15:36:18 +0200

    Exercises 7.2 - 7.5

--- books/python/Python_Programming_An_Introduction_to_Computer_Science/chapter_7/exercises.py
+++ books/python/Python_Programming_An_Introduction_to_Computer_Science/chapter_7/exercises.py
@@ -1,3 +1,4 @@
+# EXERCISE 1
 def exercise_1():
     hours = float(input("Enter the hours you worked: "))
     rate = float(input("Enter the hourly rate: "))
@@ -12,4 +13,74 @@ def exercise_1():
     except ValueError as error:
         print(error)
 
-exercise_1()
+# EXERCSE 2
+def exercise_2():
+    score = input("Enter the quizz score (1-5): ")
+
+    if score == "5":
+        print("A")
+    elif score == "4":
+        print("B")
+    elif score == "3":
+        print("C")
+    elif score == "2":
+        print("D")
+    elif score == "1":
+        print("E")
+    else:
+        print("Inalid score.")
+
+# EXERCISE 3
+def exercise_3():
+    score = int(input("Enter the quizz score (0-100): "))
+
+    if score <= 100 and score >= 90:
+        print("A")
+    elif score <= 89 and score >= 80:
+        print("B")
+    elif score <= 79 and score >= 70:
+        print("C")
+    elif score <= 69 and score >= 60:
+        print("D")
+    elif score < 60 and score >= 0:
+        print("E")
+    else:
+        print("Invalid score.")
+
+# EXERCISE 4
+def exercise_4():
+    credits = int(input("Enter your total credits: "))
+
+    if credits >= 0 and credits < 7:
+        print("You are a Freshman.")
+    if credits >= 7 and credits < 16:
+        print("You are a Sophomore.")
+    if credits >= 16 and credits < 26:
+        print("You are a Junior.")
+    if credits >= 26:
+        print("You are a Senior.")
+    else:
+        print("Invalid credits.")
+
+# EXERCISE 5
+def calculate_bmi(weight, height):
+    bmi = (weight * 720) / height ** 2
+    return bmi
+
+def exercise_5():
+    try:
+        weight = float(input("Enter your weight in pounds: "))
+        height = float(input("Enter your height in inches: "))
+        bmi = calculate_bmi(weight, height)
+    except ValueError:
+        print("Weight and height must be numbers.")
+
+
+    if bmi >= 19 and bmi <= 25:
+        print("You are within the healthy range.")
+    elif bmi < 19:
+        print("You are below the healthy range.")
+    else:
+        print("You are above the healthy range.")
+
+exercise_5()
