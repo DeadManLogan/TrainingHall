13f5a137fc7727928a6692b5c81196159f3c6042
DeadManLogan <grigoroudisdimosthenis@gmail.com>
2024-08-31 20:35:00 +0200

    Best Student

--- books/python/Python_Programming_An_Introduction_to_Computer_Science/chapter_10/practice.py
+++ books/python/Python_Programming_An_Introduction_to_Computer_Science/chapter_10/practice.py
@@ -1,5 +1,42 @@
 import math
 
+class Student:
+    def __init__(self, name, hours, qpoints):
+        self.name = name
+        self.hours = float(hours)
+        self.qpoints = float(qpoints)
+
+    def get_name(self):
+        return self.name
+    
+    def get_hours(self):
+        return self.hours
+    
+    def get_qpoints(self):
+        return self.qpoints
+    
+    def gpa(self):
+        return self.qpoints / self.hours
+    
+def make_student(info):
+    info = info.split("    ")
+    return Student(info[0], info[1], info[2])
+    
+def best_student():
+    reader = open("chapter_10/practice_material/students.txt", "r")
+
+    best_student_obj = make_student(reader.readline())
+
+    for line in reader:
+        student_obj = make_student(line)
+
+        if student_obj.gpa() > best_student_obj.gpa():
+            best_student_obj = student_obj
+    reader.close()
+
+    print(f"Best sudent: {best_student_obj.get_name()}\nGPA: {best_student_obj.gpa()}\nCredits: {best_student_obj.get_hours()}")
+
+
 class Projectile:
     def __init__(self, angle, velocity, height):
         self.x_pos = 0.0
@@ -31,5 +68,5 @@ def cannonball():
         c_ball.update(time)
     print(c_ball.get_x())
 
-cannonball()
+best_student()
 
