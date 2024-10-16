49335248db3f8ce8ac49475cbe773619a39ea075
DeadManLogan <grigoroudisdimosthenis@gmail.com>
2024-10-16 20:58:45 +0200

    Recursive functions

--- books/python/Python_Programming_An_Introduction_to_Computer_Science/chapter_13/practice_material/main.py
+++ books/python/Python_Programming_An_Introduction_to_Computer_Science/chapter_13/practice_material/main.py
@@ -4,9 +4,28 @@ def search(x, nums):
     except:
         return -1
 
+def reverse(s):
+    if s == "":
+        return s
+    else:
+        return reverse(s[1:]) + s[0]
+    
+def anagram(s):
+    if s == "":
+        return [s]
+    else:
+        ans = []
+        for word in anagram(s[1:]):
+            for position in range(len(word) + 1):
+                ans.append(word[:position] + s[0] + word[position:])
+        return ans
+
 def main():
-    nums = [1,4,2,3,6,5]
-    print(search(7, nums))
+    # nums = [1,4,2,3,6,5]
+    # print(search(7, nums))
+
+    # print(reverse("abc"))
+    print(anagram("abc"))
 
 if __name__ == "__main__":
     main()
