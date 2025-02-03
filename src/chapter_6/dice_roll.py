6390653ef0bbfc99877e9a06a5e4afba43102e5a
DeadManLogan <grigoroudisdimosthenis@gmail.com>
2025-02-03 18:13:28 +0100

    Keyword and positional arguments

--- src/chapter_6/dice_roll.py
+++ src/chapter_6/dice_roll.py
@@ -3,12 +3,22 @@ import random
 def roll_dice(sides, dice=1):
     return tuple(random.randint(1, sides) for _ in range(dice))
 
+def variadic_roll_dice(*dice):
+    if dice:
+        roll = random.randint(1, dice[0])
+        return (roll,) + variadic_roll_dice(*dice [1:])
+    return()
+
+def keyword_only_roll_dice(*, sides=6, dice=1):
+    return tuple(random.randint(1, sides) for _ in range(dice))
+
 print("Roll for initiative...")
 
-result, = roll_dice(20)
+result = keyword_only_roll_dice(sides=4, dice=2)
 print(result)
 
-# if player1 >= player2:
-#     print("Player 1")
-# else:
-#     print("Player 2")
+# result = roll_dice(20)
+# print(result)
+
+# result = variadic_roll_dice(20, 4)
+# print(callable(result))
