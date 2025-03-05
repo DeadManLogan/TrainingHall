31068d001accfb696a54d2746172f34f2d5b1c83
DeadManLogan <grigoroudisdimosthenis@gmail.com>
2025-03-05 20:48:40 +0100

    Fixed linting issues

--- src/chapter_6/lambdas.py
+++ src/chapter_6/lambdas.py
@@ -5,7 +5,7 @@ health = xp = 10
 def attempt2(action, min_roll, success_outcome, failure_outcome):
     global health, xp
     roll = random.randint(1, 20)
-    
+
     if roll >= min_roll:
         print(f"{action} SUCCEEDED.")
         result = True
@@ -14,7 +14,7 @@ def attempt2(action, min_roll, success_outcome, failure_outcome):
         print(f"{action} FAILED.")
         result = False
         health_change, xp_change = failure_outcome
-    
+
     health += health_change
     xp += xp_change
 
@@ -26,7 +26,7 @@ def attempt2(action, min_roll, success_outcome, failure_outcome):
 def attempt(action, min_roll, outcome):
     global health, xp
     roll = random.randint(1, 20)
-    
+
     if roll >= min_roll:
         print(f"{action} SUCCEEDED.")
         result = True
@@ -42,5 +42,5 @@ def attempt(action, min_roll, outcome):
     return result
 
 attempt("Eating bread", 5,
-        lambda success: (1, 0) if success else (-1, 0))
+    lambda success: (1, 0) if success else (-1, 0))
 attempt2("Fighting ice weasel", 15, (0, 10), (-10, 5))
