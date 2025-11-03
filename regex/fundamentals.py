d1a6fa4fc59112f3e6c9048486c3f46d51de69ef
DeadManLogan <grigoroudisdimosthenis@gmail.com>
2025-11-03 21:01:53 +0100

    re.findall()

---
+++ regex/fundamentals.py
@@ -0,0 +1,12 @@
+import re
+
+log = """
+[2025-11-02 08:32] System Error: 504 - Timeout
+[2025-11-02 08:33] User Error: 404 - Not Found
+[2025-11-02 08:34] Debug Info: All clear
+[2025-11-02 08:35] System Error: 500 - Internal
+"""
+# Write a regex using re.findall() to capture all error codes.
+pattern = r"Error:\s*(\d{3})" # parenthesis return only what's inside. \s* makes sure to match what comes after Error:
+res = re.findall(pattern, log)
+print(res)
