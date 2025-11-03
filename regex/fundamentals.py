6e5e94690643bc3f08871028cf79d1996d00eef8
DeadManLogan <grigoroudisdimosthenis@gmail.com>
2025-11-03 21:42:01 +0100

    Anchors and classes

--- regex/fundamentals.py
+++ regex/fundamentals.py
@@ -1,19 +1,7 @@
 import re
 
-# log = """
-# [2025-11-02 08:32] System Error: 504 - Timeout
-# [2025-11-02 08:33] User Error: 404 - Not Found
-# [2025-11-02 08:34] Debug Info: All clear
-# [2025-11-02 08:35] System Error: 500 - Internal
-# """
-# # Write a regex using re.findall() to capture all error codes.
-# pattern = r"Error:\s*(\d{3})" # parenthesis return only what's inside. \s* makes sure to match what comes after Error:
-# res = re.findall(pattern, log)
-# print(res)
+log = "Users: A123, b456, C789, D12E, E000"
+# Desired result: ['A123', 'C789', 'E000']
+pattern = r"[A-Z]\d{3}"
+print(re.findall(pattern, log))
 
-
-
-files = "sales.csv notes.txt report.csv data.log archive.csv"
-# Your goal: ['sales.csv', 'report.csv', 'archive.csv']
-pattern = r"\w+\.csv"
-print(re.findall(pattern, files))
