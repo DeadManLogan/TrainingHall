7db05afdf895e44e7a2b7bab4188b64ae968b659
DeadManLogan <grigoroudisdimosthenis@gmail.com>
2025-11-09 14:15:20 +0100

    Flags

--- regex/fundamentals.py
+++ regex/fundamentals.py
@@ -1,17 +1,29 @@
 import re
 
-# DUPLICATED WORDS
-text = "hello hello world world test"
-# 🎯 Expected result: ['hello', 'world']
-'''
-(\b\w+) → captures a full word (letters, digits, or underscore) with a word boundary before it.
+# # DUPLICATED WORDS
+# text = "hello hello world world test"
+# # 🎯 Expected result: ['hello', 'world']
+# '''
+# (\b\w+) → captures a full word (letters, digits, or underscore) with a word boundary before it.
 
-\s+ → allows any whitespace (space, tab, etc.) between words.
+# \s+ → allows any whitespace (space, tab, etc.) between words.
 
-\1 → reuses the exact word captured by the first group.
-'''
-pattern = r"(\b\w+)\s+\1"
-result = re.findall(pattern, text)
-print(result)
+# \1 → reuses the exact word captured by the first group.
+# '''
+# pattern = r"(\b\w+)\s+\1"
+# result = re.findall(pattern, text)
+# print(result)
 
 
+# FLAGS
+log = """
+ERROR: 404 - Not Found
+info: ready
+error: 500 - Timeout
+Debug: done
+"""
+
+# 🎯 Expected result: ['404', '500']
+pattern = re.compile(r"error:\s*(\d+)", re.I)
+for row in pattern.finditer(log):
+    print(row.group(1))
